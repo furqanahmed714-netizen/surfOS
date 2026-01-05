06d53dc5fdb42f98e1bd3e6508099ff59d49ce2d
furqanahmed714-netizen <furqan.ahmed714@gmail.com>
2026-01-05 19:41:13 +0500

    Updated AuthContext.tsx

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -134,6 +134,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
     if (error) return { error };
 
+    return { error: error };
   };
 
   const signOut = async () => {
