1b834dd849702686b94f31b3be3b4ec19dce7ab5
furqanahmed714-netizen <furqan.ahmed714@gmail.com>
2026-01-05 17:58:38 +0500

    Updated RetroLoginScreen.tsx

--- components/RetroLoginScreen.tsx
+++ components/RetroLoginScreen.tsx
@@ -56,7 +56,7 @@ export const RetroLoginScreen: React.FC = () => {
       
       if (subscriptionDenied) {
         setSubscriptionBlocked(true);
-        console.log(error,subscriptionDenied1);
+        console.log(error,subscriptionDenied);
         setLoading(false);
         return;
       }
